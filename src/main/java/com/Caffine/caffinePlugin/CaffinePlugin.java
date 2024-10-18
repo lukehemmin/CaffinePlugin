9d56b543d0b4cf206753a4475e2d4bb155dc2f7b
박상호 Sangho Park <ps040211@gmail.com>
2024-10-19 03:19:36 +0900

    base

---
+++ src/main/java/com/Caffine/caffinePlugin/CaffinePlugin.java
@@ -0,0 +1,17 @@
+package com.Caffine.caffinePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CaffinePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
